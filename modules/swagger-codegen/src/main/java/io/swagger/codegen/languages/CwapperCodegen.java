28a0d2a8be30faf9a7b6a249acff9718b31395c7
Dmitry Ketov <dketov@gmail.com>
2016-09-23 13:49:11 +0300

    cwapper evolved

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CwapperCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/CwapperCodegen.java
@@ -60,124 +60,123 @@ public class CwapperCodegen extends DefaultCodegen implements CodegenConfig {
 
 
     public void preprocessSwagger(Swagger swagger) {
-		Map<String, Path> swaggerPaths = swagger.getPaths();
-		
-		if(swaggerPaths == null)
-			return;
+        Map<String, Path> swaggerPaths = swagger.getPaths();
+        
+        if(swaggerPaths == null)
+            return;
 
-		for(String uri: swaggerPaths.keySet()) {
-			Path path = swaggerPaths.get(uri);
-			int paramQty = 0;
+        for(String uri: swaggerPaths.keySet()) {
+            Path path = swaggerPaths.get(uri);
+            int paramQty = 0;
 
-			path.setVendorExtension("x-cwappper-path", uri.replace("/", "_").replace("{", "").replace("}", ""));
+            path.setVendorExtension("x-cwappper-path", uri.replace("/", "_").replace("{", "").replace("}", ""));
 
-			if(path.getOperations() == null)
-				continue;
+            if(path.getOperations() == null)
+                continue;
 
-			for(Operation op: path.getOperations()) {
-				
-				if(op.getParameters() == null)
-					continue;
+            for(Operation op: path.getOperations()) {
+                
+                if(op.getParameters() == null)
+                    continue;
 
-				for(Parameter param: op.getParameters()) {
-					if(!(param instanceof PathParameter))
-						continue;
-						
-					String uriParam = String.format("{%s}", param.getName());
-					String pattern = "([^/]+)"; // type == string or whatever
-					if(((PathParameter)param).getType() == "integer")
-						pattern = "(\\\\d+)";
-					
-					if(uri.contains(uriParam)) {
-						uri = uri.replace(uriParam, pattern);
-						paramQty++;
-					}
-				}
-				
-			}
-				
-			path.setVendorExtension("x-cwappper-paramQty", paramQty);
-			path.setVendorExtension("x-cwappper-uri", uri);
-		}
-		
-		additionalProperties().put("x-cwappper-paths", swaggerPaths.values());
-	}
+                for(Parameter param: op.getParameters()) {
+                    if(!(param instanceof PathParameter))
+                        continue;
+                        
+                    String uriParam = String.format("{%s}", param.getName());
+                    String pattern = "([^/]+)"; // type == string or whatever
+                    if(((PathParameter)param).getType() == "integer")
+                        pattern = "(\\\\d+)";
+                    
+                    if(uri.contains(uriParam)) {
+                        uri = uri.replace(uriParam, pattern);
+                        paramQty++;
+                    }
+                }
+                
+            }
+                
+            path.setVendorExtension("x-cwappper-paramQty", paramQty);
+            path.setVendorExtension("x-cwappper-uri", uri);
+        }
+        
+        additionalProperties().put("x-cwappper-paths", swaggerPaths.values());
+    }
 
-	private String cwapperRE(CodegenOperation op) {
-		String RE = op.path;
-		
-		for(CodegenParameter param: op.pathParams) {
-			String pathParam = String.format("{%s}", param.baseName);
-			String pattern = "([^/]+)"; // param.isString == string or whatever
+    private String cwapperRE(CodegenOperation op) {
+        String RE = op.path;
+        
+        for(CodegenParameter param: op.pathParams) {
+            String pathParam = String.format("{%s}", param.baseName);
+            String pattern = "([^/]+)"; // param.isString == string or whatever
 
-			//if(param.isInteger || param.isLong) o_O NullPointerException ???
-				//~ pattern = "(\\\\d+)";
-				
-			//~ if(param.isFloat || param.isDouble)
-				//~ pattern = "(\\\\d*\\\\.\\\\d*)"; // FIX me
+            //if(param.isInteger || param.isLong) o_O NullPointerException ???
+                //~ pattern = "(\\\\d+)";
+                
+            //~ if(param.isFloat || param.isDouble)
+                //~ pattern = "(\\\\d*\\\\.\\\\d*)"; // FIX me
 
-			if(RE.contains(pathParam))
-				RE = RE.replace(pathParam, pattern);
-		}
-		
-		return RE;
-	}
-						
+            if(RE.contains(pathParam))
+                RE = RE.replace(pathParam, pattern);
+        }
+        
+        return RE;
+    }
+                        
     public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
         Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
         if (operations == null)
-			return objs;
-		
-		List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
-		if (ops == null)
-			return objs;
-		
-		class CwapperPath {
-			public String path, re, fid;
+            return objs;
+        
+        List<CodegenOperation> ops = (List<CodegenOperation>) operations.get("operation");
+        if (ops == null)
+            return objs;
+        
+        class CwapperPath {
+            public String path, re, fid;
 
-			public List<CodegenOperation> operations = new ArrayList<CodegenOperation>();
-			private Set<String> pathParams = new HashSet<String>();
-				
-			CwapperPath(String _path, String _re) {
-				path = _path;
-				re = _re;
-				fid = path.replace("/", "_").replace("{", "").replace("}", "");
-			}
-			public String toString() {
-				return String.format("%s->%s", path, operations.toString());
-			}
-			public boolean hasPathParams() { return !pathParams.isEmpty(); }
+            public List<CodegenOperation> operations = new ArrayList<CodegenOperation>();
+            public List<CodegenParameter> pathParams = null;
+                
+            CwapperPath(String _path, String _re) {
+                path = _path;
+                re = _re;
+                fid = path.replace("/", "_").replace("{", "").replace("}", "");
+            }
+            public String toString() {
+                return String.format("%s->%s", path, operations.toString());
+            }
+            public boolean hasPathParams() { return !pathParams.isEmpty(); }
 
-			public void add(CodegenOperation op) {	
-				LOGGER.warn(op.toString());
-				LOGGER.warn(op.pathParams.toString());
-				for(CodegenParameter p: op.pathParams)
-					pathParams.add(p.baseName);
-			
-				operations.add(op);
-			}
-		}
-		
-		Map<String, CwapperPath> cwapperMap = new HashMap<String, CwapperPath> ();
-			
-		for(CodegenOperation operation : ops) {
-			String re = cwapperRE(operation);
-			CwapperPath p = cwapperMap.get(operation.path);
-			
-			if(p == null) {
-				p = new CwapperPath(operation.path, re);
-				cwapperMap.put(operation.path, p);
-			}
-			
-			p.add(operation);
-		}
+            public void add(CodegenOperation op) {  
+                //~ LOGGER.warn(op.toString());
+                //~ LOGGER.warn(op.pathParams.toString());
+                pathParams = op.pathParams;
+            
+                operations.add(op);
+            }
+        }
+        
+        Map<String, CwapperPath> cwapperMap = new HashMap<String, CwapperPath> ();
+            
+        for(CodegenOperation operation : ops) {
+            String re = cwapperRE(operation);
+            CwapperPath p = cwapperMap.get(operation.path);
+            
+            if(p == null) {
+                p = new CwapperPath(operation.path, re);
+                cwapperMap.put(operation.path, p);
+            }
+            
+            p.add(operation);
+        }
 
-		List<CwapperPath> xcwapper = (List<CwapperPath>)additionalProperties().get("x-cwapper");
-		if(xcwapper == null)
-			additionalProperties().put("x-cwapper", new ArrayList<CwapperPath>(cwapperMap.values()));
-		else
-			xcwapper.addAll(cwapperMap.values());
-		
+        List<CwapperPath> xcwapper = (List<CwapperPath>)additionalProperties().get("x-cwapper");
+        if(xcwapper == null)
+            additionalProperties().put("x-cwapper", new ArrayList<CwapperPath>(cwapperMap.values()));
+        else
+            xcwapper.addAll(cwapperMap.values());
+        
         LOGGER.warn(cwapperMap.entrySet().toString());
         return objs;
     }      
